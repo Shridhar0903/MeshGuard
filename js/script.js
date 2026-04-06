42c4e9788f4761f4339bac02aabdc0be54c9d1f8
Shridhar0903 <bagayatkarshridhar@gmail.com>
2026-04-06 16:12:41 +0530

    updated Landing Page

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+
+// ===================================================================================
+
+const cards = document.querySelectorAll(".process-card");
+
+window.addEventListener("scroll", () => {
+  const trigger = window.innerHeight * 0.85;
+
+  cards.forEach((card) => {
+    const top = card.getBoundingClientRect().top;
+
+    if (top < trigger) {
+      card.classList.add("show");
+    }
+  });
+});
